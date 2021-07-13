48a79c81843ec4585b038d56d4d794ed87f6e4db
Bartłomiej Krakowski <bartlomiej@krakowski.dev>
2021-07-13 23:02:02 +0200

    feat: introduced the singleton pattern

---
+++ singleton/index.ts
@@ -0,0 +1,18 @@
+class Singleton {
+  private static instance: Singleton
+
+  private constructor() {}
+
+  public static getInstance(): Singleton {
+    if (!Singleton.instance) {
+      Singleton.instance = new Singleton()
+    }
+
+    return Singleton.instance
+  }
+}
+
+// ----------------------------------------------------------------
+
+const s1 = Singleton.getInstance()
+const s2 = Singleton.getInstance()
